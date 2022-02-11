90dd2f353613d5ee89c6464050c9b7a7be3f6cc2
Dennis Priskorn <68460690+dpriskorn@users.noreply.github.com>
2022-02-11 15:36:42 +0100

    Export also pickles for contributors and affiliations

--- models/extractor.py
+++ models/extractor.py
@@ -26,6 +26,8 @@ class Extractor(BaseModel):
     """
     swepub_deduplicated_zipfile_path: str = None
     article_pickle_filename: str = "articles.pkl.gz"
+    affiliations_pickle_filename: str = "affiliations.pkl.gz"
+    contributors_pickle_filename: str = "contributors.pkl.gz"
     subjects_pickle_filename: str = "subjects.pkl.gz"
     stop_line_number: int = config.stop_line_number
     start_line_number: int = config.start_line_number
@@ -40,6 +42,8 @@ class Extractor(BaseModel):
         start = time.time()
         articles_df = pd.DataFrame()
         subjects_df = pd.DataFrame()
+        contributors_df = pd.DataFrame()
+        affiliations_df = pd.DataFrame()
         # This probably read the whole thing into memory which is not ideal...
         with zipfile.ZipFile(self.swepub_deduplicated_zipfile_path) as z:
             for filename in z.namelist():
@@ -58,7 +62,7 @@ class Extractor(BaseModel):
                                       f"progress{progress}%",
                                       flush=True)
                             if current_line_number >= self.start_line_number:
-                                article = SwepubArticle(raw_data=line)
+                                article: SwepubArticle = SwepubArticle(raw_data=line)
                                 df_article = article.export_dataframe()
                                 if article.subjects is not None:
                                     df_subject = pd.DataFrame()
@@ -71,6 +75,31 @@ class Extractor(BaseModel):
                                         print(df_subject.describe())
                                         exit()
                                     subjects_df = subjects_df.append(df_subject)
+                                if article.contributors is not None:
+                                    df_contributors = pd.DataFrame()
+                                    for contributor in article.contributors:
+                                        df_contributors = df_contributors.append(contributor.export_dataframe())
+                                        if contributor.affiliations is not None:
+                                            df_affiliations = pd.DataFrame()
+                                            for affiliation in contributor.affiliations:
+                                                df_affiliations = df_affiliations.append(affiliation.export_dataframe())
+                                            # add article id as index to all rows
+                                            # https://www.geeksforgeeks.org/add-column-with-constant-value-to-pandas-dataframe/
+                                            df_affiliations['id'] = pd.Series(
+                                                [article.id for x in range(len(df_affiliations.index))]
+                                            )
+                                            if config.loglevel == logging.DEBUG:
+                                                print(df_affiliations.describe())
+                                                exit()
+                                            affiliations_df = affiliations_df.append(df_affiliations)
+                                    # add article id as index to all rows
+                                    # https://www.geeksforgeeks.org/add-column-with-constant-value-to-pandas-dataframe/
+                                    df_contributors['id'] = pd.Series(
+                                        [article.id for x in range(len(df_contributors.index))])
+                                    if config.loglevel == logging.DEBUG:
+                                        print(df_contributors.describe())
+                                        exit()
+                                    contributors_df = contributors_df.append(df_contributors)
                                 articles_df = articles_df.append(df_article)
                             if current_line_number == self.stop_line_number:
                                 logger.warning("Reached stop line number")
@@ -82,5 +111,7 @@ class Extractor(BaseModel):
         print(f"starting to save subjects pickle {self.article_pickle_filename} now", flush=True)
         subjects_df.to_pickle(self.subjects_pickle_filename, protocol=5)
         print(f"saved to pickle {self.subjects_pickle_filename}", flush=True)
+        contributors_df.to_pickle(self.contributors_pickle_filename)
+        affiliations_df.to_pickle(self.affiliations_pickle_filename)
         end = time.time()
         print(f"total duration: {round(end - start)}s")
